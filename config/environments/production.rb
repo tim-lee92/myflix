ba5e7e7732c09b672387b202e63f03ed2bba9e63
tim-lee92 <tim-lee92@hotmail.com>
2017-04-11 17:19:47 -0700

    modified production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'myflix-timlee92.heroku.com' }
+
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
